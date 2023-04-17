197755ec05b803ad2f58cfa2ad8739eef53b6056
kaf <897243839@qq.com>
2023-04-17 14:14:15 +0800

    Update hcdata.go

--- hcdata.go
+++ hcdata.go
@@ -10,21 +10,11 @@ var (
 	Maphot                      = New[int]()
 	MapLit                      = New[int]()
 	Mode         CompressorType = 1
+	Tsf                         = make(chan string)
+	Num                         = 1000000
 )
 
 // var maps sync.RWMutex
 // var myTimer = time.Now().Unix() // 启动定时器
 var Ticker = time.NewTicker(60 * time.Second)     //计时器
 var Ticker1 = time.NewTicker(30000 * time.Minute) //计时器
-
-var Cb = func(exists bool, valueInMap int, newValue int) int {
-	if !exists {
-		return newValue
-	}
-	if valueInMap > 999 {
-		return 1000
-	}
-	valueInMap += newValue
-	return valueInMap
-
-}
